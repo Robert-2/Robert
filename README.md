322fc04407705814d85d617f21e5d21a3efc2926
polosson <polosson@EtoileNoire>
2013-01-06 17:16:26 +0100

    LICENCE ajoutée

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-robert
-======
 
-Gestion de parc de matériel, techniciens, calendrier...
+* ROBERT, version 0.5
+
+
+** Desciption rapide du Robert
+===========================
+
+Robert, c'est :
+
+* Une WEB-APP (logiciel en ligne) open source, écrite en php, js, html et xml, utilisant jQuery, ajax et mysql.
+
+Hum, mais encore ?
+
+* Un gestionnaire de parc de matériel destiné à la location
+* Un outil bien pratique pour assurer le bon fonctionnement d'un parc de matériel et la liaison avec les clients / techniciens, accèssible n'importe où depuis internet
+* Un moyen simple et efficace de s'y retrouver dans les sorties-retours de matériel
+* Une interface claire et fonctionnelle
+* Une solution pour stocker les données relative au parc, de manière sécurisée et centralisée sur votre propre serveur
+* Un projet communautaire auquel tout le monde (quidam, association, entreprise) peut participer
+
+
+Robert, ce n'est pas :
+
+* Un logiciel de compta
+* Un logiciel éxécutable sur un ordinateur local (c'est une web-app, accessible seulement via un navigateur web)
+* Un substitut à votre cerveau
+* Une machine à café
+
+
+
+** Documentation du Robert
+=======================
+
+* Pour commencer		_TODO_
+* Pour aller + loin	_TODO_
+* Pour contribuer		_TODO_
+* Pour développer		_TODO_
+
+
+** Liste des fonctionnalités du Robert
+===================================
+
+
+** Licence du Robert
+=================
+
+Le Robert est un logiciel libre; vous pouvez le redistribuer et/ou
+le modifier sous les termes de la Licence Publique Générale GNU Affero
+comme publiée par la Free Software Foundation;
+version 3.0.
+
+Cette WebApp est distribuée dans l'espoir qu'elle soit utile,
+mais SANS AUCUNE GARANTIE; sans même la garantie implicite de
+COMMERCIALISATION ou D'ADAPTATION A UN USAGE PARTICULIER.
+Voir la Licence Publique Générale GNU Affero pour plus de détails.
+
+Vous devriez avoir reçu une copie de la Licence Publique Générale
+GNU Affero avec les sources du logiciel (LICENCE.txt); si ce n'est pas
+le cas, rendez-vous à http://www.gnu.org/licenses/agpl.txt (en Anglais)
+
--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <?php
+/*
+ *
+    Le Robert est un logiciel libre; vous pouvez le redistribuer et/ou
+    le modifier sous les termes de la Licence Publique Générale GNU Affero
+    comme publiée par la Free Software Foundation;
+    version 3.0.
+
+    Cette WebApp est distribuée dans l'espoir qu'elle soit utile,
+    mais SANS AUCUNE GARANTIE; sans même la garantie implicite de
+	COMMERCIALISATION ou D'ADAPTATION A UN USAGE PARTICULIER.
+	Voir la Licence Publique Générale GNU Affero pour plus de détails.
+
+    Vous devriez avoir reçu une copie de la Licence Publique Générale
+	GNU Affero avec les sources du logiciel; si ce n'est pas le cas,
+	rendez-vous à http://www.gnu.org/licenses/agpl.txt (en Anglais)
+ *
+ */
 	session_start();
-	require_once ('initInclude.php');
-	require_once ('common.inc');		// OBLIGATOIRE pour les sessions, à placer TOUJOURS EN HAUT du code !!
+	require_once ('initInclude.php');		// OBLIGATOIRE pour les sessions,
+	require_once ('common.inc');			// à placer TOUJOURS EN HAUT du code.
 	require_once ('checkConnect.php' );
-    
+
 	$titrePageBar = "ROBERT";
 	include('inc/head_html.php');
 
 ?>
 
 <body>
-	
+
 	<div id="bigDiv">
 		<?php
-		
+
 		?>
 		<div id="Page" class="fondPage bordPage">
-			
+
             <div class="colonne L bordSection ui-widget ui-corner-all fondSect1 center">
 				<?php
 				if ( isset($_SESSION["user"])) {
@@ -28,7 +45,7 @@
 					$page_admin = array ('calendrier', 'materiel', 'gens', 'beneficiaires', 'infos', 'sauvegarde' );
 					$page_users = array ('calendrier', 'materiel', 'gens', 'beneficiaires' );
 					$choose = array();
-					
+
 					if ( $_SESSION['user']->isAdmin() ) $choose = $page_admin ;  else $choose = $page_users ;
 
 					foreach ( $choose as $k ){
@@ -53,12 +70,12 @@
 				<span class="boutonMenu petit noMarge" title="Trucs qu'il nous reste à faire">
 					<a href="todos.php"><b>TODOs</b></a><br />
 				</span>
-				
+
 
 			</div>
 
-			
-			
+
+
 			<div class="colonne C bordSection ui-widget ui-corner-all fondSect2 petit">
 				<?	if ( !isset($_SESSION['user']) ) include ('modals/connexion.php');
 					else {
@@ -74,21 +91,21 @@
 					}
 				?>
 			</div>
-			
-			
+
+
 			<div class="colonne R bordSection ui-widget ui-corner-all fondSect1 petit center">
-				<? 
+				<?
 				if ($logged == true) {
 					include('menuRight.php');
 				}
 				?>
-				
+
 			</div>
-			
+
 		</div>
 
 		<? // include('pages/footPage.php'); ?>
-		
+
 	</div>
 </body>
 </html>
