afaf8872623a5b8ce940e9cc3cb698a6173efc89
Paul MAILLARDET <polo@polosson.com>
2017-11-12 02:12:28 +0100

    Refactore le meteo_parser

--- fct/meteo_parser.php
+++ fct/meteo_parser.php
@@ -8,15 +8,15 @@ if (!isset($location) && !isset($date)) {
 
 $retour = [];
 
-$date_wanted= date_create($date);
+$date_wanted = date_create($date);
 $time_wanted = $date_wanted->format('U');
 
 $date_curr = date_create();
 $date_curr->setTime(0, 0, 0);
 $time_current = $date_curr->format('U');
 
-$location = urlencode($location);
-$xmlWeatherAdr = 'http://api.meteorologic.net/forecarss?p='.$location ;
+$location      = urlencode($location);
+$xmlWeatherAdr = 'http://api.meteorologic.net/forecarss?p=' . $location ;
 
 // CURL
 $curl = curl_init(); // Initialiser cURL.
@@ -25,28 +25,34 @@ curl_setopt($curl, CURLOPT_HEADER, 0); // Ne pas inclure l'header dans la répon
 ob_start(); // Commencer à 'cache' l'output.
 curl_exec($curl); // Exécuter la requète.
 curl_close($curl); // Fermer cURL.
-$xmlStr = ob_get_contents(); // Sauvegarder le 'cache' dans la variable $Results.
+$xmlStr = ob_get_contents(); // Sauvegarder le 'cache' dans la variable $xmlStr.
 ob_end_clean(); // Vider le buffer.
 
-$gotXmlTag = preg_match('/^\<\?xml/', $xmlStr);
-
-if ($gotXmlTag > 0) {
-    $xml = simplexml_load_string(utf8_encode($xmlStr));
-    $item = $xml->channel->item;
-    $previz = [];
-    foreach ($item->xpath('//meteo:weather') as $tag) {
-        $desc = utf8_decode((string)$tag['namepictos_apmidi']);
-        $previz[] = [
-            'icon'      => (string)$tag['pictos_apmidi'],
-            'condition' => $desc,
-            'temp'      => (string)$tag['tempe_apmidi'].'°'
-        ];
-    }
-    $previz['err'] = [
-        'icon'      => 'inconnu',
-        'condition' => "Impossible de trouver la météo... C'est dans trop longtemps, ou bien c'est déjà passé !",
-        'temp'      => '?'
+if (preg_match('/^\<\?xml/', $xmlStr)) {
+    $xml    = simplexml_load_string(utf8_encode($xmlStr));
+    $item   = $xml->channel->item;
+    $previz = [
+        'err' => [
+            'icon'      => 'inconnu',
+            'condition' => "Impossible de trouver la météo pour cette ville... " .
+                           "C'est dans trop longtemps, ou bien c'est déjà passé !",
+            'temp'      => '?'
+        ]
     ];
+
+    if (preg_match('/\<meteo:weather /', $xmlStr)) {
+        $tags = $item->xpath('//meteo:weather');
+        if (is_array($tags) && !empty($tags)) {
+            foreach ($tags as $tag) {
+                $desc = utf8_decode((string)$tag['namepictos_apmidi']);
+                $previz[] = [
+                    'icon'      => (string)$tag['pictos_apmidi'],
+                    'condition' => $desc,
+                    'temp'      => (string)$tag['tempe_apmidi'].'°'
+                ];
+            }
+        }
+    }
 
     $timeOffset = $time_wanted - $time_current;
     $dateOffset = number_format($timeOffset / (3600 * 24), 0);
@@ -57,9 +63,9 @@ if ($gotXmlTag > 0) {
     }
 } else {
     $retour = [
-        "day_of_week" => "inconnu",
-        "icon"        => "gfx\/icones\/unknown_weather.png",
-        "condition"   => "Impossible de trouver la m\u00e9t\u00e9o... Meteorologic ne répond pas, ou mal."
+        "icon"      => "inconnu",
+        "condition" => "Impossible de trouver la météo... Meteorologic ne répond pas, ou mal.",
+        "temp"      => "inconnu"
     ];
 }
 echo json_encode($retour);
